7f2a0a27d8e0a05b43a026fd9a724220f68c80bf
vitaliypopel <vitaliypopel14@gmail.com>
2024-08-01 13:17:42 +0300

    created models

--- django_quiz/quiz/admin.py
+++ django_quiz/quiz/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+admin.site.register(
+    models.Quiz,
+    models.Question,
+    models.Choice,
+    models.UserAnswer,
+)
--- django_quiz/quiz/models.py
+++ django_quiz/quiz/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=100)
+    descriptions = models.CharField(max_length=2000)
+    url_title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'quizzes'
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = 'questions'
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    is_correct = models.BooleanField()
+
+    class Meta:
+        db_table = 'choices'
+
+    def __str__(self):
+        return self.text
+
+
+class UserAnswer(models.Model):
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    session = models.CharField(max_length=300)
+
+    class Meta:
+        db_table = 'user_answers'
