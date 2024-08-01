5d39cb79a0ab81d0f9c3fb6b11cd0cfb8181cc8b
vitaliypopel <vitaliypopel14@gmail.com>
2024-08-01 15:12:15 +0300

    added GET views and templates

--- django_quiz/quiz/urls.py
+++ django_quiz/quiz/urls.py
@@ -5,5 +5,19 @@ from . import views
 
 app_name = 'quiz'
 urlpatterns = [
-
+    path(
+        '',
+        views.QuizzesView.as_view(),
+        name='quizzes'
+    ),
+    path(
+        '<slug:quiz_title>/',
+        views.QuizView.as_view(),
+        name='quiz'
+    ),
+    path(
+        '<slug:quiz_title>/<int:question_id>/',
+        views.QuizQuestionView.as_view(),
+        name='question'
+    ),
 ]
--- django_quiz/quiz/views.py
+++ django_quiz/quiz/views.py
@@ -1,1 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.views.generic import View
+
+from .models import Quiz, Question, Choice, UserAnswer
+
+
+class QuizzesView(View):
+    def get(self, request):
+        quizzes = Quiz.objects.all()
+
+        return render(
+            request,
+            template_name='quiz/quizzes.html',
+            context={'quizzes': quizzes},
+        )
+
+
+class QuizView(View):
+    def get(self, request, quiz_title: str):
+        quiz = Quiz.objects.get(url_title=quiz_title)
+
+        return render(
+            request,
+            template_name='quiz/quiz.html',
+            context={'quiz': quiz},
+        )
+
+
+class QuizQuestionView(View):
+    def get(self, request, quiz_title: str, question_id: int):
+        quiz = Quiz.objects.get(url_title=quiz_title)
+        question = Question.objects.get(quiz=quiz, pk=question_id)
+        choices = Choice.objects.filter(question=question)
+
+        return render(
+            request,
+            template_name='quiz/question.html',
+            context={
+                'quiz': quiz,
+                'question': question,
+                'choices': choices,
+                'question_id': question_id,
+            },
+        )
