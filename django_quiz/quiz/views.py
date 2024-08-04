f65d7bc8d62cba4285cb9631f23bcc2e78bcde2d
vitaliypopel <vitaliypopel14@gmail.com>
2024-08-04 13:46:31 +0300

    require methods decorators was added

--- django_quiz/quiz/views.py
+++ django_quiz/quiz/views.py
@@ -1,21 +1,26 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404, get_list_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.http import require_GET, require_POST
 from django.views.generic import View, ListView, DetailView, RedirectView
 
 from .models import Quiz, Question, Choice, Answer, CompletedQuiz
 from .utils import get_user_session
 
 
+@method_decorator(require_GET, name='dispatch')
 class HomeView(RedirectView):
     def get_redirect_url(self, *args, **kwargs):
         return reverse('quiz:quizzes')
 
 
+@method_decorator(require_GET, name='dispatch')
 class QuizzesView(ListView):
     template_name = 'quiz/quizzes.html'
     model = Quiz
     context_object_name = 'quizzes'
 
 
+@method_decorator(require_GET, name='dispatch')
 class QuizView(DetailView):
     template_name = 'quiz/quiz.html'
     model = Quiz
@@ -25,16 +30,19 @@ class QuizView(DetailView):
         return get_object_or_404(self.model, url_title=self.kwargs['quiz_title'])
 
 
+@method_decorator(require_GET, name='dispatch')
 class QuizQuestionView(View):
     def get(self, request, quiz_title: str, question_id: int):
         ...
 
 
+@method_decorator(require_POST, name='dispatch')
 class QuizQuestionChoiceView(View):
     def post(self, request, quiz_title: str, question_id: int, choice_id: int):
         ...
 
 
+@method_decorator(require_GET, name='dispatch')
 class QuizResultView(View):
     def get(self, request, quiz_title: str):
         quiz = get_object_or_404(Quiz, url_title=quiz_title)
@@ -55,6 +63,7 @@ class QuizResultView(View):
         )
 
 
+@method_decorator(require_GET, name='dispatch')
 class DashboardView(View):
     def get(self, request):
         user_session = get_user_session(request)
