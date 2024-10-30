f23433725dda4ea717b70b68c6ba57feac34becf
vitaliypopel <vitaliypopel14@gmail.com>
2024-10-30 17:58:08 +0200

    Renamed project to "project"

---
+++ project/quiz/models.py
@@ -0,0 +1,71 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Quiz(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    descriptions = models.CharField(max_length=2000)
+    complexity = models.IntegerField(
+        choices=(
+            (1, 'Easy'),
+            (2, 'Medium'),
+            (3, 'Hard'),
+        ),
+    )
+    url_title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+        db_table = 'quizzes'
+
+    def __str__(self):
+        return self.title
+
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    number = models.IntegerField()
+    text = models.CharField(max_length=300)
+    is_last = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'questions'
+
+    def __str__(self):
+        return self.text
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=300)
+    is_correct = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'choices'
+
+    def __str__(self):
+        return self.text
+
+
+class Answer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'user_answers'
+
+
+class CompletedQuiz(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    result = models.IntegerField(default=0)
+    is_completed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = 'Completed quizzes'
+        db_table = 'user_completed_quizzes'
---
+++ project/quiz/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+
+from . import views
+
+
+app_name = 'quiz'
+urlpatterns = [
+    path('',
+         views.HomeView.as_view(), name='home'),
+    path('dashboard/',
+         views.DashboardView.as_view(), name='dashboard'),
+    path('quizzes/',
+         views.QuizzesView.as_view(), name='quizzes'),
+    path('quizzes/<slug:quiz_title>/',
+         views.QuizView.as_view(), name='quiz'),
+    path('quizzes/<slug:quiz_title>/<int:question_id>/',
+         views.QuizQuestionView.as_view(), name='question'),
+    path('quizzes/<slug:quiz_title>/<int:question_id>/answer/',
+         views.QuizAnswerView.as_view(), name='answer'),
+    path('quizzes/<slug:quiz_title>/<int:question_id>/complete/',
+         views.QuizCompleteView.as_view(), name='complete'),
+    path('quizzes/<slug:quiz_title>/result/',
+         views.QuizResultView.as_view(), name='result'),
+    path('quizzes/<slug:quiz_title>/again/',
+         views.QuizTakeAgainView.as_view(), name='again'),
+]
